247ba5b9edd8f22d14814ea962bf1d0920974594
Iankkkk <yanifoughali08@gmail.com>
2019-09-23 11:57:15 +0200

    created link with heroku

--- server.js
+++ server.js
@@ -1,1 +1,14 @@
-console.log("Hello World");
+const express = require('express');
+
+var port = process.env.port || 8080
+
+let app = express();
+
+app.get('/', (req, res) => 
+{
+    res.write("Hello World");
+    res.end();
+}
+)
+
+app.listen(8080);
