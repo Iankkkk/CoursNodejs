6acdc61536b84c60d69a16089f118156673860ce
Iankkkk <yanifoughali08@gmail.com>
2019-09-23 12:36:38 +0200

    Added firt phrases to our chatbot

--- server.js
+++ server.js
@@ -1,14 +1,26 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 var port = process.env.PORT || 3000
 
 let app = express();
 
-app.get('/', (req, res) => 
-{
-    res.write("Hello World");
-    res.end();
-}
-)
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+    res.send("Hello World");
+})
+
+app.post('/chat', (req, res) => {
+    obj = JSON.parse(JSON.stringify(req.body,null," "));
+
+    if(obj.msg == "ville"){
+        res.send("Nous sommes a Paris");
+    }
+    if(obj.msg == "meteo"){
+        res.send("Il fait beau aujourd'hui");
+    }
+
+})
 
 app.listen(port);
